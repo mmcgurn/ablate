48d099702032724fec6475ba34eae608b00710ce
matt mcgurn <matt@mcgurn.dev>
2021-07-22 14:01:35 -0400

    lib update

--- ablateLibrary/parser/yamlParser.hpp
+++ ablateLibrary/parser/yamlParser.hpp
@@ -107,7 +107,7 @@ class YamlParser : public Factory {
     /* get all children as factory */
     std::vector<std::shared_ptr<Factory>> GetFactorySequence(const std::string& name) const override;
 
-    bool Contains(const std::string& name) const override { return yamlConfiguration[name] != nullptr; };
+    bool Contains(const std::string& name) const override { return yamlConfiguration[name].IsNull(); };
 
     std::unordered_set<std::string> GetKeys() const override;
 
